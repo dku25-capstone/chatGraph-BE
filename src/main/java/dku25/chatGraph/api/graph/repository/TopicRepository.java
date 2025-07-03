345582f9e0626a1048cb0f0e1a6dd81397b220a0
jkli51 <jkk307@naver.com>
2025-07-03 11:44:47 +0900

    fix:sync

--- src/main/java/dku25/chatGraph/api/graph/repository/TopicRepository.java
+++ src/main/java/dku25/chatGraph/api/graph/repository/TopicRepository.java
@@ -2,6 +2,12 @@ package dku25.chatGraph.api.graph.repository;
 
 import dku25.chatGraph.api.graph.node.TopicNode;
 import org.springframework.data.neo4j.repository.Neo4jRepository;
+import org.springframework.data.neo4j.repository.query.Query;
+
+import java.util.List;
 
 public interface TopicRepository extends Neo4jRepository<TopicNode, String> {
+
+    @Query("MATCH (t:topic) Return t ORDER BY t.createdAt DESC")
+    List<TopicNode> findBySessionId(String sessionId);
 }
