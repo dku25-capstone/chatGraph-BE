5ec6f2ca38cf5daa3ac567da57eb841569e09a40
jun562 <capboy290@naver.com>
2025-07-05 20:00:11 +0900

    feat(jwt): jwt 토큰 생성, 검증 방식 구현

--- src/main/java/dku25/chatGraph/api/util/JwtUtil.java
+++ src/main/java/dku25/chatGraph/api/util/JwtUtil.java
@@ -1,7 +1,17 @@
 package dku25.chatGraph.api.util;
 
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Date;
+
+import javax.crypto.spec.SecretKeySpec;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.JwtException;
 
 @Component
 public class JwtUtil {
@@ -9,4 +19,38 @@ public class JwtUtil {
   private String secretkey;
   @Value("${jwt.expiration}")
   private long expirationMs;
+
+  public String generateToken(String email, String role) {
+    Date now = new Date();
+    Date expiryDate = new Date(now.getTime() + expirationMs);
+    Key key = new SecretKeySpec(secretkey.getBytes(StandardCharsets.UTF_8), SignatureAlgorithm.HS256.getJcaName());
+    return Jwts.builder()
+            .setSubject(email)
+            .claim("role", role)
+            .setIssuedAt(now)
+            .setExpiration(expiryDate)
+            .signWith(key, SignatureAlgorithm.HS256)
+            .compact();
+  } //토큰 생성
+
+  public String getEmailFromToken(String token) {
+    return Jwts.parserBuilder()
+            .setSigningKey(secretkey.getBytes(StandardCharsets.UTF_8))
+            .build()
+            .parseClaimsJws(token)
+            .getBody()
+            .getSubject();
+  } // 토큰에서 이메일 추출
+
+  public boolean validateToken(String token) {
+    try {
+        Jwts.parserBuilder()
+            .setSigningKey(secretkey.getBytes(StandardCharsets.UTF_8))
+            .build()
+            .parseClaimsJws(token);
+        return true;
+    } catch (JwtException | IllegalArgumentException e) {
+        return false;
+    }
+  }// 토큰 유효성 검증
 }
