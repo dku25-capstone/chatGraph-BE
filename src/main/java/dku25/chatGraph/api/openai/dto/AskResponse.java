96dbc1f5f99620a549f5d483eff63e46d2415a52
jkli51 <jkk307@naver.com>
2025-07-10 22:25:06 +0900

    feat:add askResponse, fix js, html

---
+++ src/main/java/dku25/chatGraph/api/openai/dto/AskResponse.java
@@ -0,0 +1,12 @@
+package dku25.chatGraph.api.openai.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+@AllArgsConstructor
+public class AskResponse {
+    private String answer;
+    private String questionId;
+}
