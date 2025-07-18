eb58880c244957f8a21bd4d65966087681553106
jkli51 <jkk307@naver.com>
2025-07-18 17:54:44 +0900

    feat:TopicService

--- src/main/java/dku25/chatGraph/api/controller/GraphController.java
+++ src/main/java/dku25/chatGraph/api/controller/GraphController.java
@@ -1,12 +1,11 @@
 package dku25.chatGraph.api.controller;
 
+import dku25.chatGraph.api.graph.dto.RenameTopicRequestDTO;
 import dku25.chatGraph.api.graph.dto.TopicResponseDTO;
 import dku25.chatGraph.api.graph.dto.QuestionAnswerDTO;
 import dku25.chatGraph.api.graph.service.GraphService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
 import java.util.List;
@@ -29,10 +28,30 @@ public class GraphController {
 
     @GetMapping("/topics/{topicId}")
     public ResponseEntity<List<QuestionAnswerDTO>> getTopicQuestions(
-            @PathVariable String topicId, 
+            @PathVariable String topicId,
             Principal principal) {
         String userId = principal.getName();
         List<QuestionAnswerDTO> questions = graphService.getTopicQuestionsAndAnswers(topicId, userId);
         return ResponseEntity.ok(questions);
+    }
+
+    @PatchMapping("/topics/{topicId}")
+    public ResponseEntity<TopicResponseDTO> renameTopic(
+            @PathVariable String topicId,
+            Principal principal,
+            @RequestBody RenameTopicRequestDTO req) {
+        String userId = principal.getName();
+        String newTopicName = req.getNewTopicName();
+        TopicResponseDTO topic = graphService.renameTopic(topicId, userId, newTopicName);
+        return ResponseEntity.ok(topic);
+    }
+
+    @DeleteMapping("/topics/{topicId}")
+    public ResponseEntity<Void> deleteTopic(
+            @PathVariable String topicId,
+            Principal principal) {
+        String userId = principal.getName();
+        graphService.deleteTopic(topicId, userId);
+        return ResponseEntity.noContent().build();
     }
 } 
