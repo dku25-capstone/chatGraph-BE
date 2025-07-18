9c1e164e4d88e945bbd5bf596b934af7533b247c
jkli51 <jkk307@naver.com>
2025-07-18 17:54:44 +0900

    feat:TopicService

---
+++ src/main/java/dku25/chatGraph/api/graph/dto/RenameTopicRequestDTO.java
@@ -0,0 +1,9 @@
+package dku25.chatGraph.api.graph.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class RenameTopicRequestDTO {
+    private String newTopicName;
+}
--- src/main/java/dku25/chatGraph/api/graph/repository/TopicRepository.java
+++ src/main/java/dku25/chatGraph/api/graph/repository/TopicRepository.java
@@ -1,5 +1,6 @@
 package dku25.chatGraph.api.graph.repository;
 
+import dku25.chatGraph.api.graph.dto.TopicResponseDTO;
 import dku25.chatGraph.api.graph.node.TopicNode;
 import dku25.chatGraph.api.graph.dto.QuestionAnswerDTO;
 import org.springframework.data.neo4j.repository.Neo4jRepository;
@@ -9,10 +10,23 @@ import java.util.List;
 
 public interface TopicRepository extends Neo4jRepository<TopicNode, String> {
     @Query("MATCH (t:Topic {topicId: $topicId})-[:start_conversation]->(q:Question) " +
-           "OPTIONAL MATCH (q)-[:FOLLOWED_BY*0..]->(allQ:Question) " +
-           "OPTIONAL MATCH (allQ)-[:HAS_ANSWER]->(allA:Answer) " +
-           "RETURN allQ.questionId AS questionId, allQ.text AS questionText, allQ.level AS level, " +
-           "allA.answerId AS answerId, allA.text AS answerText, allQ.createdAt AS createdAt " +
-           "ORDER BY level")
+            "OPTIONAL MATCH (q)-[:FOLLOWED_BY*0..]->(allQ:Question) " +
+            "OPTIONAL MATCH (allQ)-[:HAS_ANSWER]->(allA:Answer) " +
+            "RETURN allQ.questionId AS questionId, allQ.text AS questionText, allQ.level AS level, " +
+            "allA.answerId AS answerId, allA.text AS answerText, allQ.createdAt AS createdAt " +
+            "ORDER BY level")
     List<QuestionAnswerDTO> findQuestionsAndAnswersByTopicId(String topicId);
+
+    @Query("""
+              MATCH (t:Topic {topicId: $topicId})
+              SET t.topicName = $newTopicName
+              RETURN t
+            """)
+    TopicResponseDTO renameTopic(String topicId, String newTopicName);
+
+    @Query("""
+            MATCH (t:Topic {topicId: $topicId})
+            DETACH DELETE t
+            """)
+    void deleteById(String topicId);
 }
