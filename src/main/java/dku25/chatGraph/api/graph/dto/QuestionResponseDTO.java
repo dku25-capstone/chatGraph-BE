53760ea19d42a950e49d2e8cd5ac5c42d63ba636
jun562 <capboy290@naver.com>
2025-07-20 01:17:30 +0900

    feat(question_rename): 질문 노드 이름 수정 구현

---
+++ src/main/java/dku25/chatGraph/api/graph/dto/QuestionResponseDTO.java
@@ -0,0 +1,10 @@
+package dku25.chatGraph.api.graph.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class QuestionResponseDTO {
+    private String questionId;
+    private String text;
+} 
