853b03fe6875456ade6765e3845534ab3f02e1ac
jun562 <capboy290@naver.com>
2025-07-21 18:22:42 +0900

    refactor(question): 문답노드 삭제 cypher문으로 리팩토링

--- src/main/java/dku25/chatGraph/api/graph/repository/QuestionRepository.java
+++ src/main/java/dku25/chatGraph/api/graph/repository/QuestionRepository.java
@@ -1,6 +1,23 @@
 package dku25.chatGraph.api.graph.repository;
 
 import dku25.chatGraph.api.graph.node.QuestionNode;
-import org.springframework.data.neo4j.repository.Neo4jRepository;
 
-public interface QuestionRepository extends Neo4jRepository<QuestionNode, String> {}
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.neo4j.repository.Neo4jRepository;
+import org.springframework.data.neo4j.repository.query.Query;
+
+public interface QuestionRepository extends Neo4jRepository<QuestionNode, String> {
+  //  모든 자식노드 조회
+  @Query("MATCH (q:Question)-[:PREVIOUS_QUESTION]->(parent:Question {questionId: $parentId}) RETURN q")
+  List<QuestionNode> findChildrenByParentId(String parentId);
+
+  // 부모 - 특정자식노드 관계 삭제
+  @Query("MATCH (parent:Question {questionId: $parentId})-[r:FOLLOWED_BY]->(child:Question {questionId: $childId}) DELETE r")
+  void removeFollowedByRelation(String parentId, String childId);
+
+  // 부모 - 특정자식노드 관계 설정
+  @Query("MATCH (parent:Question {questionId: $parentId}), (child:Question {questionId: $childId}) MERGE (parent)-[:FOLLOWED_BY]->(child)")
+  void createFollowedByRelation(String parentId, String childId);
+}
