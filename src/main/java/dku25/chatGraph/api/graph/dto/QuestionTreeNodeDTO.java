1aed37bd6d5693f51fc2b8e55c8867fca76151b0
jkli51 <jkk307@naver.com>
2025-07-28 01:38:08 +0900

    feat/TreeResponse

--- src/main/java/dku25/chatGraph/api/graph/dto/QuestionTreeNodeDTO.java
+++ src/main/java/dku25/chatGraph/api/graph/dto/QuestionTreeNodeDTO.java
@@ -1,31 +1,29 @@
 package dku25.chatGraph.api.graph.dto;
 
-import java.time.LocalDateTime;
-import java.util.List;
-
 import lombok.Getter;
 import lombok.ToString;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Getter
 @ToString
-public class QuestionAnswerDTO {
+public class QuestionTreeNodeDTO {
     private final String questionId;
     private final String question;
     private final int level;
     private final String answerId;
     private final String answer;
     private final LocalDateTime createdAt;
-    private final String parentId;
-    private final List<String> children;
+    private final List<QuestionTreeNodeDTO> children;
 
-    public QuestionAnswerDTO(String questionId, String question, int level, String answerId, String answer, LocalDateTime createdAt, String parentId, List<String> children) {
+    public QuestionTreeNodeDTO(String questionId, String question, int level, String answerId, String answer, LocalDateTime createdAt, List<QuestionTreeNodeDTO> children) {
         this.questionId = questionId;
         this.question = question;
         this.level = level;
         this.answerId = answerId;
         this.answer = answer;
         this.createdAt = createdAt;
-        this.parentId = parentId;
         this.children = children;
     }
-} 
+}
