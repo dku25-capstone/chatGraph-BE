2437b22d0f2fa1d0946d6b107462d246e717921f
jkli51 <jkk307@naver.com>
2025-11-16 19:52:03 +0900

    mod:isFavorite attribute

--- src/main/java/dku25/chatGraph/api/graph/dto/QuestionAnswerDTO.java
+++ src/main/java/dku25/chatGraph/api/graph/dto/QuestionAnswerDTO.java
@@ -12,15 +12,17 @@ public class QuestionAnswerDTO {
     private final String questionId;
     private final String questionText;
     private final int level;
+    private final boolean isFavorite;
     private final String answerId;
     private final String answerText;
     private final LocalDateTime createdAt;
     private final List<String> children;
 
-    public QuestionAnswerDTO(String questionId, String questionText, int level, String answerId, String answerText, LocalDateTime createdAt, List<String> children) {
+    public QuestionAnswerDTO(String questionId, String questionText, int level,boolean isFavorite, String answerId, String answerText, LocalDateTime createdAt, List<String> children) {
         this.questionId = questionId;
         this.questionText = questionText;
         this.level = level;
+        this.isFavorite = isFavorite;
         this.answerId = answerId;
         this.answerText = answerText;
         this.createdAt = createdAt;
